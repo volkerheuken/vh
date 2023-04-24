3fd49447add4f09ac07ce7e9da8c0756a9a97ed7
klausHe <klausheuken@gmx.de>
2023-04-24 22:02:07 +0200

    Instrument select spielt richtiges Instrument ab

--- Ocjene/Ocjene.js
+++ Ocjene/Ocjene.js
@@ -10,7 +10,7 @@
 const ocjeneSubgrid = [
 	["cl_OcjeneSheet", "right", "start"],
 	["cl_OcjeneLevelHeader", "left", "end"],
-  ["cl_OcjeneLevelselect1", "left"],
+	["cl_OcjeneLevelselect1", "left"],
 	["cl_OcjeneLevelselect2", "left"],
 	["cl_OcjeneSongHeader", "left", "end"],
 	["cl_OcjeneRhytmusHeader", "left", "end"],
@@ -477,10 +477,142 @@ const ocjeneInstruments = {
 			upper: Math.min(this.instrument.range.upper, base + ocjeneOptions.variables.rangeOffset.val),
 		};
 	},
+	// midi: [
+	// 	[0, "acoustic_grand_piano"],
+	// 	[1, "bright_acoustic_piano"],
+	// 	[2, "electric_grand_piano"],
+	// 	[3, "honkytonk_piano"],
+	// 	[4, "electric_piano_1"],
+	// 	[5, "electric_piano_2"],
+	// 	[6, "harpsichord"],
+	// 	[7, "clavinet"],
+	// 	[8, "celesta"],
+	// 	[9, "glockenspiel"],
+	// 	[10, "music_box"],
+	// 	[11, "vibraphone"],
+	// 	[12, "marimba"],
+	// 	[13, "xylophone"],
+	// 	[14, "tubular_bells"],
+	// 	[15, "dulcimer"],
+	// 	[16, "drawbar_organ"],
+	// 	[17, "percussive_organ"],
+	// 	[18, "rock_organ"],
+	// 	[19, "church_organ"],
+	// 	[20, "reed_organ"],
+	// 	[21, "accordion"],
+	// 	[22, "harmonica"],
+	// 	[23, "tango_accordion"],
+	// 	[24, "acoustic_guitar_nylon"],
+	// 	[25, "acoustic_guitar_steel"],
+	// 	[26, "electric_guitar_jazz"],
+	// 	[27, "electric_guitar_clean"],
+	// 	[28, "electric_guitar_muted"],
+	// 	[29, "overdriven_guitar"],
+	// 	[30, "distortion_guitar"],
+	// 	[31, "guitar_harmonics"],
+	// 	[32, "acoustic_bass"],
+	// 	[33, "electric_bass_finger"],
+	// 	[34, "electric_bass_pick"],
+	// 	[35, "fretless_bass"],
+	// 	[36, "slap_bass_1"],
+	// 	[37, "slap_bass_2"],
+	// 	[38, "synth_bass_1"],
+	// 	[39, "synth_bass_2"],
+	// 	[40, "violin"],
+	// 	[41, "viola"],
+	// 	[42, "cello"],
+	// 	[43, "contrabass"],
+	// 	[44, "tremolo_strings"],
+	// 	[45, "pizzicato_strings"],
+	// 	[46, "orchestral_harp"],
+	// 	[47, "timpani"],
+	// 	[48, "string_ensemble_1"],
+	// 	[49, "string_ensemble_2"],
+	// 	[50, "synth_strings_1"],
+	// 	[51, "synth_strings_2"],
+	// 	[52, "choir_aahs"],
+	// 	[53, "voice_oohs"],
+	// 	[54, "synth_choir"],
+	// 	[55, "orchestra_hit"],
+	// 	[56, "trumpet"],
+	// 	[57, "trombone"],
+	// 	[58, "tuba"],
+	// 	[59, "muted_trumpet"],
+	// 	[60, "french_horn"],
+	// 	[61, "brass_section"],
+	// 	[62, "synth_brass_1"],
+	// 	[63, "synth_brass_2"],
+	// 	[64, "soprano_sax"],
+	// 	[65, "alto_sax"],
+	// 	[66, "tenor_sax"],
+	// 	[67, "baritone_sax"],
+	// 	[68, "oboe"],
+	// 	[69, "english_horn"],
+	// 	[70, "bassoon"],
+	// 	[71, "clarinet"],
+	// 	[72, "piccolo"],
+	// 	[73, "flute"],
+	// 	[74, "recorder"],
+	// 	[75, "pan_flute"],
+	// 	[76, "blown_bottle"],
+	// 	[77, "shakuhachi"],
+	// 	[78, "whistle"],
+	// 	[79, "ocarina"],
+	// 	[80, "lead_1_square"],
+	// 	[81, "lead_2_sawtooth"],
+	// 	[82, "lead_3_calliope"],
+	// 	[83, "lead_4_chiff"],
+	// 	[84, "lead_5_charang"],
+	// 	[85, "lead_6_voice"],
+	// 	[86, "lead_7_fifths"],
+	// 	[87, "lead_8_bass_lead"],
+	// 	[88, "pad_1_new_age"],
+	// 	[89, "pad_2_warm"],
+	// 	[90, "pad_3_polysynth"],
+	// 	[91, "pad_4_choir"],
+	// 	[92, "pad_5_bowed"],
+	// 	[93, "pad_6_metallic"],
+	// 	[94, "pad_7_halo"],
+	// 	[95, "pad_8_sweep"],
+	// 	[96, "fx_1_rain"],
+	// 	[97, "fx_2_soundtrack"],
+	// 	[98, "fx_3_crystal"],
+	// 	[99, "fx_4_atmosphere"],
+	// 	[100, "fx_5_brightness"],
+	// 	[101, "fx_6_goblins"],
+	// 	[102, "fx_7_echoes"],
+	// 	[103, "fx_8_scifi"],
+	// 	[104, "sitar"],
+	// 	[105, "banjo"],
+	// 	[106, "shamisen"],
+	// 	[107, "koto"],
+	// 	[108, "kalimba"],
+	// 	[109, "bagpipe"],
+	// 	[110, "fiddle"],
+	// 	[111, "shanai"],
+	// 	[112, "tinkle_bell"],
+	// 	[113, "agogo"],
+	// 	[114, "steel_drums"],
+	// 	[115, "woodblock"],
+	// 	[116, "taiko_drum"],
+	// 	[117, "melodic_tom"],
+	// 	[118, "synth_drum"],
+	// 	[119, "reverse_cymbal"],
+	// 	[120, "guitar_fret_noise"],
+	// 	[121, "breath_noise"],
+	// 	[122, "seashore"],
+	// 	[123, "bird_tweet"],
+	// 	[124, "telephone_ring"],
+	// 	[125, "helicopter"],
+	// 	[126, "applause"],
+	// 	[127, "gunshot"],
+	// 	[128, "percussion"],
+	// ],
 	data: [
 		{
 			Name: "Klavier",
 			group: "Strings",
+			midiInstrumentIndex: 0,
 			range: {
 				lower: 33,
 				upper: 108,
@@ -490,6 +622,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Geige",
 			group: "Strings",
+			midiInstrumentIndex: 40,
 			range: {
 				lower: 55,
 				upper: 103,
@@ -499,6 +632,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Viola",
 			group: "Strings",
+			midiInstrumentIndex: 41,
 			range: {
 				lower: 48,
 				upper: 91,
@@ -508,6 +642,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Cello",
 			group: "Strings",
+			midiInstrumentIndex: 42,
 			range: {
 				lower: 36,
 				upper: 76,
@@ -517,6 +652,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Kontrabass",
 			group: "Strings",
+			midiInstrumentIndex: 43,
 			range: {
 				lower: 28,
 				upper: 67,
@@ -526,6 +662,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Bassgitarre",
 			group: "Strings",
+			midiInstrumentIndex: 32,
 			range: {
 				lower: 28,
 				upper: 67,
@@ -535,6 +672,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Gitarre",
 			group: "Strings",
+			midiInstrumentIndex: 24,
 			range: {
 				lower: 40,
 				upper: 88,
@@ -544,6 +682,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Tuba",
 			group: "Brass",
+			midiInstrumentIndex: 58,
 			range: {
 				lower: 28,
 				upper: 58,
@@ -553,6 +692,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Bassposaune",
 			group: "Brass",
+			midiInstrumentIndex: 57,
 			range: {
 				lower: 34,
 				upper: 67,
@@ -562,6 +702,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Posaune",
 			group: "Brass",
+			midiInstrumentIndex: 57,
 			range: {
 				lower: 40,
 				upper: 72,
@@ -571,6 +712,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Flügelhorn",
 			group: "Brass",
+			midiInstrumentIndex: 57,
 			range: {
 				lower: 34,
 				upper: 77,
@@ -580,6 +722,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Trompete",
 			group: "Brass",
+			midiInstrumentIndex: 56,
 			range: {
 				lower: 55,
 				upper: 82,
@@ -589,6 +732,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Piccoloflöte",
 			group: "Woodwinds",
+			midiInstrumentIndex: 72,
 			range: {
 				lower: 74,
 				upper: 102,
@@ -598,6 +742,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Flöte",
 			group: "Woodwinds",
+			midiInstrumentIndex: 74,
 			range: {
 				lower: 60,
 				upper: 96,
@@ -607,6 +752,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Oboe",
 			group: "Woodwinds",
+			midiInstrumentIndex: 68,
 			range: {
 				lower: 58,
 				upper: 91,
@@ -616,6 +762,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Altflöte",
 			group: "Woodwinds",
+			midiInstrumentIndex: 68,
 			range: {
 				lower: 55,
 				upper: 91,
@@ -625,6 +772,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Englischhorn",
 			group: "Woodwinds",
+			midiInstrumentIndex: 69,
 			range: {
 				lower: 52,
 				upper: 81,
@@ -634,6 +782,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Klarinette",
 			group: "Woodwinds",
+			midiInstrumentIndex: 71,
 			range: {
 				lower: 50,
 				upper: 94,
@@ -643,6 +792,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Bassklarinette",
 			group: "Woodwinds",
+			midiInstrumentIndex: 71,
 			range: {
 				lower: 38,
 				upper: 77,
@@ -652,6 +802,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Fagott",
 			group: "Woodwinds",
+			midiInstrumentIndex: 70,
 			range: {
 				lower: 34,
 				upper: 75,
@@ -661,6 +812,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Kontrafagott",
 			group: "Woodwinds",
+			midiInstrumentIndex: 70,
 			range: {
 				lower: 22,
 				upper: 53,
@@ -670,6 +822,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Sopranblockflöte",
 			group: "Woodwinds",
+			midiInstrumentIndex: 74,
 			range: {
 				lower: 72,
 				upper: 98,
@@ -679,6 +832,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Altblockflöte",
 			group: "Woodwinds",
+			midiInstrumentIndex: 74,
 			range: {
 				lower: 65,
 				upper: 91,
@@ -688,6 +842,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Tenorblockflöte",
 			group: "Woodwinds",
+			midiInstrumentIndex: 74,
 			range: {
 				lower: 60,
 				upper: 86,
@@ -697,6 +852,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Bassblockflöte",
 			group: "Woodwinds",
+			midiInstrumentIndex: 74,
 			range: {
 				lower: 53,
 				upper: 79,
@@ -706,6 +862,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Baritonsaxophon",
 			group: "Woodwinds",
+			midiInstrumentIndex: 67,
 			range: {
 				lower: 36,
 				upper: 69,
@@ -715,6 +872,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Tenorsaxophon",
 			group: "Woodwinds",
+			midiInstrumentIndex: 66,
 			range: {
 				lower: 44,
 				upper: 76,
@@ -724,6 +882,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Altsaxophon",
 			group: "Woodwinds",
+			midiInstrumentIndex: 65,
 			range: {
 				lower: 49,
 				upper: 81,
@@ -733,6 +892,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Sopranosaxophon",
 			group: "Woodwinds",
+			midiInstrumentIndex: 64,
 			range: {
 				lower: 56,
 				upper: 88,
@@ -742,6 +902,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Glockenspiel",
 			group: "Percussion",
+			midiInstrumentIndex: 9,
 			range: {
 				lower: 79,
 				upper: 108,
@@ -751,6 +912,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Xylophon",
 			group: "Percussion",
+			midiInstrumentIndex: 13,
 			range: {
 				lower: 65,
 				upper: 108,
@@ -760,6 +922,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Vibraphon",
 			group: "Percussion",
+			midiInstrumentIndex: 11,
 			range: {
 				lower: 53,
 				upper: 89,
@@ -769,6 +932,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Marimba",
 			group: "Percussion",
+			midiInstrumentIndex: 12,
 			range: {
 				lower: 45,
 				upper: 96,
@@ -778,6 +942,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Bass Marimba",
 			group: "Percussion",
+			midiInstrumentIndex: 12,
 			range: {
 				lower: 33,
 				upper: 81,
@@ -787,6 +952,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Celesta",
 			group: "Percussion",
+			midiInstrumentIndex: 8,
 			range: {
 				lower: 60,
 				upper: 108,
@@ -796,6 +962,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Röhrenglocken",
 			group: "Percussion",
+			midiInstrumentIndex: 14,
 			range: {
 				lower: 60,
 				upper: 77,
@@ -805,6 +972,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Pauken",
 			group: "Percussion",
+			midiInstrumentIndex: 47,
 			range: {
 				lower: 40,
 				upper: 55,
@@ -814,6 +982,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Cembalo",
 			group: "Percussion",
+			midiInstrumentIndex: 6,
 			range: {
 				lower: 29,
 				upper: 89,
@@ -823,6 +992,7 @@ const ocjeneInstruments = {
 		{
 			Name: "Harfe",
 			group: "Percussion",
+			midiInstrumentIndex: 46,
 			range: {
 				lower: 24,
 				upper: 103,
@@ -891,6 +1061,9 @@ const ocjeneSong = {
 		displayPlay: true,
 		displayProgress: true,
 		displayWarp: false,
+		get program() {
+			return ocjeneInstruments.instrument.midiInstrumentIndex;
+		},
 		get drumIntro() {
 			return 0; //ocjeneOptions.metronome.val;
 		},
